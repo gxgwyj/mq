8803b3ce780f7239a24cae4d596d2338b99898f9
gaoxugang1 <1253652573@qq.com>
2017-02-06 15:18:25 +0800

    update  controller

--- mq/src/main/java/com/xinzheng/mq/controller/MqController.java
+++ mq/src/main/java/com/xinzheng/mq/controller/MqController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * rabbitMq消息传递
+ * rabbitMq消息传递(联想用户修改)
  * @author Administrator
  *
  */
