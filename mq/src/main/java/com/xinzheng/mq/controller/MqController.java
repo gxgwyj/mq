c05b91e89d4686e44df49ec36132b3c24d9820cb
= <498868779@qq.com>
2017-02-06 11:42:15 +0800

    update  controller

--- mq/src/main/java/com/xinzheng/mq/controller/MqController.java
+++ mq/src/main/java/com/xinzheng/mq/controller/MqController.java
@@ -3,6 +3,11 @@ package com.xinzheng.mq.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * rabbitMq消息传递
+ * @author Administrator
+ *
+ */
 @Controller
 public class MqController {
 	 @RequestMapping(value = "/main")
